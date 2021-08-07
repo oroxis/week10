dcdfb4f192c81f9001f1c7fb992fb854c2ad629b
oroxis <85342801+oroxis@users.noreply.github.com>
2021-08-07 11:39:21 +0100

    Add files via upload

---
+++ class/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Homepage() {
+    return (
+        <div>
+            <div>
+                <h1>FRONT END</h1>
+                <h1>DEVELOPMENT</h1>
+                <button>MORE</button>
+                <h2>THIS IS THE HOME PAGE</h2>
+
+
+
+            </div>
+        </div>
+    )
+}
---
+++ class/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <section style={{display: 'flex'}}>
+                <p>HOME</p> <br />
+                <p>ABOUT</p> <br />
+                <p>CONTACT</p> <br />
+                <p>LOGIN</p> <br />
+
+
+            </section>
+        </div>
+    )
+}
